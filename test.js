b1f52a5200d8b79507d9ac13468b9fe4de97a287
mehlingerf <fabienmehlinger.pro@gmail.com>
2016-05-02 15:03:16 +0200

    calculatrice

---
+++ test.js
@@ -0,0 +1,26 @@
+function rslt()
+{	
+	var nombr1 = document.forms["calculatrice"].nb1.value;
+	var nombr2 = document.forms["calculatrice"].nb2.value;
+	var resultat;
+	
+	switch (document.calculatrice.operateur.value)
+	{
+		case '+':
+		resultat =parseFloat(nombr1) + parseFloat(nombr2) ;
+		break;
+		case '-':
+		resultat =parseFloat(nombr1) - parseFloat(nombr2);
+		break;
+		case '*':
+		resultat =parseFloat(nombr1) * parseFloat(nombr2);
+		break;
+		case '/':
+		resultat =parseFloat(nombr1) / parseFloat(nombr2);
+		break;
+		default: 
+		document.calculatrice.mess.value = "Verifier votre operation";
+		break;
+	}
+	document.forms["calculatrice"].ecran.value = resultat;
+}
